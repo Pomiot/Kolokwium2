817f18b02a6e87d82884eae2aea2acd51e7728ab
plbo <pawel.baszuro@gft.com>
2015-05-26 22:07:54 +0200

    dodanie zadania

---
+++ MessagePublisher/MessagesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePublisher
+{
+    /// <summary>
+    /// Your TODO: please follow insstruction 
+    /// </summary>
+    public class MessagesViewModel
+    {
+        private readonly IDispatcher _dispatcher;
+
+        public MessagesViewModel(IDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;            
+        }
+
+    }
+}
